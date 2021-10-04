7a410d25a7e53c704e82af515ec2c99573eff62f
Hridoy-31 <raihanulbashirhridoy@gmail.com>
2021-10-04 16:25:50 +0600

    Implementation of cursor moving with arrow keys

--- texty.c
+++ texty.c
@@ -1,14 +1,24 @@
 #include <ctype.h> 
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h> 
 #include <termios.h> 
 #include <unistd.h> 
 #include <errno.h> 
-#include <sys/ioctl.h> 
+#include <sys/ioctl.h>
+#include <string.h>
 
+#define CODIBLE_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP, 
+  ARROW_DOWN 
+};
+
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencolumns;
   struct termios original;
@@ -47,13 +57,33 @@ void enableRawMode() {
   }
 }
 
-char editorReadKey() {
+int editorReadKey() {
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if (nread == -1 && errno != EAGAIN) {
       die("read");
     }
+  }
+  if (c=='\x1b') {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) {
+      return '\x1b';
+    }
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) {
+      return '\x1b';
+    }
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+      case 'A' : return ARROW_UP;
+      case 'B' : return ARROW_DOWN;
+      case 'C' : return ARROW_RIGHT;
+      case 'D' : return ARROW_LEFT;
+      }
+    }
+    return '\x1b';
+  }
+  else {
     return c;
   }
 }
@@ -98,32 +128,89 @@ int getWindowSize(int *rows, int *columns) {
   }
 }
 
-void editorDrawRows() {
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+  if (new == NULL) {
+    return;
+  }
+  memcpy(&new[ab->len],s,len); 
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
+}
+
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y=0; y<E.screenrows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    abAppend(ab, "~", 1);
+    abAppend(ab, "\x1b[K", 3);
+    if (y < E.screenrows-1) {
+      abAppend(ab, "\r\n", 2);
+    }
   }
 }
 
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
-  editorDrawRows();
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
+  abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
+  editorDrawRows(&ab);
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy+1, E.cx+1);
+  abAppend(&ab, buf, strlen(buf));
+  abAppend(&ab, "\x1b[?25h", 6);
+  write(STDOUT_FILENO, ab.b, ab.len); 
+  abFree(&ab); 
+}
+
+void editorMoveCursor (int key) {
+  switch (key) {
+  case ARROW_LEFT:
+    E.cx--;
+    break;
+  case ARROW_RIGHT:
+    E.cx++;
+    break;
+  case ARROW_UP:
+    E.cy--;
+    break;
+  case ARROW_DOWN:
+    E.cy++;
+    break;
+  }
 }
 
 void editorProcessKeypress() {
-  char c = editorReadKey();
+  int c = editorReadKey();
   switch (c) {
   case CTRL_KEY('q'):
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
     break;
+
+  case ARROW_UP:
+  case ARROW_DOWN:
+  case ARROW_LEFT:
+  case ARROW_RIGHT:
+    editorMoveCursor(c);
+    break;
   }
 }
 
 void initialEditor() {
+  E.cx = 0;
+  E.cy = 0; 
   if (getWindowSize(&E.screenrows, &E.screencolumns)==-1) {
     die("getWindowSize");
   }
@@ -146,7 +233,7 @@ int main()
       printf("%d\r\n", c);
     }
     else {
-      printf("%d ('%c')\r\n", c, c); 
+      printf("%d ('%c')\r\n", c, c);
     }
     if (c == CTRL_KEY('q'))
     {
